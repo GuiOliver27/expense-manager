6d2ef4ca436139931ec5f1ab302ef69b0a200ae2
GuiOliver <guilherme_oliveira27@outlook.com>
2022-07-08 19:33:17 -0300

    Update and development of new features

---
+++ src/data/categories.ts
@@ -0,0 +1,7 @@
+import { Category } from '../types/Category'
+
+export const categories: Category = {
+    food: { title: 'Alimentação', color: '#0000FF', expense: true },
+    rent: { title: 'Aluguel', color: '#8B4513', expense: true },
+    salary: { title: 'Salário', color: '#00FA9A', expense: false }
+}
---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date(2022, 7, 6), category: 'food', title: 'McDonalds', value: 31.12 },
+    { date: new Date(2022, 7, 6), category: 'food', title: 'Burger King', value: 29 },
+    { date: new Date(2022, 7, 6), category: 'rent', title: 'Aluguel Apt', value: 1500 },
+    { date: new Date(2022, 6, 20), category: 'salary', title: 'Gui Inc', value: 7800 },
+]
